82c26b421173397dbfb70cb45a357bc6a7c87e40
AveryAlexander <51173726+AveryAlexander@users.noreply.github.com>
2026-02-17 22:33:46 -0500

    Create curlme.sh

---
+++ curlme.sh
@@ -0,0 +1,9 @@
+#
+# Welcome to Alex's Bash Starting Point!
+#
+# No features have been added yet.
+# Debian quick custom-config script planned.
+#
+# To test quick execution, type "!!|bash" to run the following:
+#
+curl xmas.ysap.sh | bash
