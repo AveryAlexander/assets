98d0afcb60bd21b8d49662b4e3bdcbf98e459ea2
AveryAlexander <51173726+AveryAlexander@users.noreply.github.com>
2026-02-17 23:16:25 -0500

    Update curlme.sh

--- curlme.sh
+++ curlme.sh
@@ -5,5 +5,12 @@
 # Debian quick custom-config script planned.
 #
 # To test quick execution, type "!!|bash" to run the following:
-#
-curl xmas.ysap.sh | bash
+
+# curl xmas.ysap.sh | bash
+
+wget https://raw.githubusercontent.com/AveryAlexander/assets/refs/heads/main/firstrun_customize_260217_2305.sh
+mv ./firstrun_customize_260217_2305.sh ./firstrun.sh
+echo
+echo type " nano firstrun.sh " to review and edit code
+echo
+echo type " cat firstrun.sh | sudo bash " to run
